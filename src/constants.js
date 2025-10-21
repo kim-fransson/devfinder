b62cc7adc816b282920e2333930e2f26724552a4
Kim Fransson <kim.fransson.22@gmail.com>
2025-10-21 20:32:49 +0200

    Project setup

---
+++ src/constants.js
@@ -0,0 +1,3 @@
+export const SITE_TITLE = "devfinder";
+export const SITE_DESCRIPTION =
+  "DevFinder lets you search and explore GitHub user profiles instantly. View repositories, followers, activity stats, and more — all in a sleek, dark (or light) themed interface built for developers.";
